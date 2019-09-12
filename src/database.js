591f58089a21d1abe4ca093125b9c3054e8e2345
Jose Alberto RT <jramireztolentino@gmail.com>
2019-09-12 09:41:33 -0400

    Modificacion a la ruta DB

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://jramirez:jramirez@noteapp-db-iaehc.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://jramirez:jramirez@noteapp-db-iaehc.mongodb.net/test?retryWrites=true&w=majority', {
         useCreateIndex: true,
         useNewUrlParser: true,
         useFindAndModify: false
