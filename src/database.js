5f7236aac0b7b205c946f61efde0a21dabf41d0c
Jose Alberto RT <jramireztolentino@gmail.com>
2019-09-12 10:50:12 -0400

    Segunda Modificacion de coneccion a base de datos

--- src/database.js
+++ src/database.js
@@ -7,3 +7,12 @@ mongoose.connect('mongodb+srv://jramirez:jramirez@noteapp-db-iaehc.mongodb.net/t
     })
     .then(db => console.log('DB is Conecting'))
     .catch(err => console.error(err));
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://jramirez:jramirez@noteapp-db-iaehc.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     // perform actions on the collection object
+//     client.close();
+// });
